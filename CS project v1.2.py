39db2e5d48dc14155df2d4a0edcf81ef21ce8431
Vedant Patil <142657913+VedantPatil99@users.noreply.github.com>
2023-09-02 15:00:44 +0530

    Update CS project v1.2.py

--- CS project v1.2.py
+++ CS project v1.2.py
@@ -288,7 +288,7 @@ while True:
                     msg= otp
                     s = smtplib.SMTP('smtp.gmail.com', 587)
                     s.starttls()
-                    s.login("xxxxxxxxxxxxxxx@gmail.com", "xxxxxxxxxxx") #Enter the email you want to send mails with and type in the app password 
+                    s.login("___________________@gmail.com", "_____________") #Enter the email you want to send mails with and type in the app password 
         
                     s.sendmail('&&&&&&&&&&&',email,msg)
                     
@@ -383,7 +383,7 @@ while True:
             msg= otp
             s = smtplib.SMTP('smtp.gmail.com', 587)
             s.starttls()
-            s.login("xxxxxxxxxxxxxxxxxxxxxx@gmail.com", "xxxxxxxxxxxxxxxxxx") #Enter the email you want to send mails with and type in the app password 
+            s.login("_________________@gmail.com", "_________________") #Enter the email you want to send mails with and type in the app password 
             emailid = input("Confirm your email: ")
             s.sendmail('&&&&&&&&&&&',emailid,msg)
             otp = input("Enter Your OTP >>: ")
@@ -543,7 +543,7 @@ while True:
                         msg= otp
                         s = smtplib.SMTP('smtp.gmail.com', 587)
                         s.starttls()
-                        s.login("xxxxxxxxxxxx@gmail.com", "xxxxxxxxxxxxxxx") #Enter the email you want to send mails with and type in the app password 
+                        s.login("_________________@gmail.com", "___________________") #Enter the email you want to send mails with and type in the app password 
             
                         s.sendmail('&&&&&&&&&&&',email,msg)
                         otp = input("Enter Your OTP >>: ")
